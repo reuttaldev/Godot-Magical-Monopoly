28617d48a0635482510889c561bc0ac2d590c657
Reut Tal <reutgaming@gmail.com>
2022-06-02 15:21:16 +0200

    Changing editor to vs code and configuring main menu button

---
+++ Scripts/UI/MenuController.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+public class MenuController : Node
+{
+    [Signal]
+    // this is connected to the script of the main game 
+    public delegate void StartGame();
+    public void OnClick()
+    {
+        // change to the board scene
+        GD.Print("start button is being pressed ");
+        GetTree().ChangeScene("res://Scenes/BoardScene.tscn");
+        //EmitSignal("StartGame");
+    }
+}
---
+++ Scripts/UI/notes.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+/*
+public void UpdateScore(int score)
+{
+    GetNode<Label>("ScoreLabel").Text = score.ToString();
+}
+
+timer use timer node
+    await ToSignal(GetTree().CreateTimer(1), "timeout");
+    GetNode<Button>("StartButton").Show();
+*/
